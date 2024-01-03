616351e58e614858ec13440d7f09f4c2fabef8a9
mwguidry <37812815+mwguidry@users.noreply.github.com>
2024-01-03 14:56:48 -0700

    PE-472: updated regex

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 shopt -s nocasematch
 
 # Define the JIRA ticket pattern
-JIRA_TICKET_PATTERN="(FRBI|CRI|IR|MREG|MVKPLTFRM|MVKENG|MAVRCK|RF|REV|CIAM)-[0-9]+"
+JIRA_TICKET_PATTERN="[A-Z]{2,}-[0-9]+"
 
 # Get the PR number and repo information from the environment variables
 PR_NUMBER=${PR_NUMBER}
